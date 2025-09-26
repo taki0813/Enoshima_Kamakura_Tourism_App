4cf1ac202d821448c675c7474fa074dfbc860ec5
regulus13 <a23.65by@g.chuo-u.ac.jp>
2025-09-26 15:41:07 +0900

    動的レンダリング

--- app/admin/spots/page.tsx
+++ app/admin/spots/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect, useMemo } from "react"
 import { useRouter } from "next/navigation"
 import { useSession } from "next-auth/react"

22c8efd4b47764748c864259999e30f19b91ec5c
regulus13 <a23.65by@g.chuo-u.ac.jp>
2025-09-26 15:23:06 +0900

    prisma変更

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-const prisma = global.prisma || new PrismaClient()
+// PrismaClientの新しいインスタンスを返す関数
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+// グローバルオブジェクトに`prisma`プロパティが存在することを宣言
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default prisma
+// グローバルオブジェクトの`prisma`が存在すればそれを使用し、なければ新しいインスタンスを作成
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+// 本来のexport default prisma; の代わりに、名前付きエクスポートに変更
+// これにより、`import { prisma } from '@/lib/prisma'` が機能する
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
+
+export { prisma };
